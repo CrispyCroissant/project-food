34c1d4447847a9c66b7a5d09379b132c5dafea0c
CrispyCroissant <gentlemug00@gmail.com>
2021-12-17 17:57:59 +0100

    Added auth router to app config

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const app = express();
 require("./db/db");
 
+const authRouter = require("./routes/auth");
+
 app.use(express());
 app.use(cors());
+
+app.use("/api", authRouter);
 
 module.exports = app;
