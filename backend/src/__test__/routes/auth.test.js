6c7e6509b26f160b3e963e00e9f5f6eb0b855d1a
CrispyCroissant <gentlemug00@gmail.com>
2021-12-17 19:07:56 +0100

    Implemented user creation on /register

--- backend/src/__test__/routes/auth.test.js
+++ backend/src/__test__/routes/auth.test.js
@@ -1,14 +1,70 @@
 const request = require("supertest");
+const mockingoose = require("mockingoose");
 const app = require("../../app");
 
 describe("POST /api/register", () => {
     const route = "/api/register";
 
     it("should exist", async () => {
-        const res = await request(app).post(route);
+        const creds = {
+            email: "correct@email.com",
+            password: "password123",
+        };
+
+        const res = await request(app).post(route).send(creds);
 
         expect(res).toBeDefined();
         expect(res.status).toBe(200);
+    });
+
+    it("should return error if email is not given", async () => {
+        const creds = {
+            password: "password123",
+        };
+
+        const res = await request(app).post(route).send(creds);
+
+        expect(res).toBeDefined();
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe("Email is required!");
+    });
+
+    it("should return error if password is not given", async () => {
+        const creds = {
+            email: "correct@email.com",
+        };
+
+        const res = await request(app).post(route).send(creds);
+
+        expect(res).toBeDefined();
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe("Password is required!");
+    });
+
+    it("should return status 200 if everything went OK", async () => {
+        const creds = {
+            email: "correct@email.com",
+            password: "password132",
+        };
+
+        const res = await request(app).post(route).send(creds);
+
+        expect(res).toBeDefined();
+        expect(res.status).toBe(200);
+        expect(res.body.status).toBe(200);
+    });
+
+    it("should return status 400 if user could not be created", async () => {
+        const creds = {
+            email: "correctcom",
+            password: "password132",
+        };
+
+        const res = await request(app).post(route).send(creds);
+
+        expect(res).toBeDefined();
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBeDefined();
     });
 });
 
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,7 +3,25 @@ const router = express.Router();
 const User = require("../models/user");
 
 router.post("/register", async (req, res) => {
-    res.send();
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).send({ error: "Email is required!" });
+    }
+
+    if (!password) {
+        return res.status(400).send({ error: "Password is required!" });
+    }
+
+    const user = new User({ email, password, emailConfirmed: false });
+
+    try {
+        await user.save();
+    } catch (error) {
+        return res.status(400).send({ error: error.message });
+    }
+
+    res.send({ status: 200 });
 });
 
 router.post("/login", async (req, res) => {
