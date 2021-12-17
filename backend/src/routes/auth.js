e7083afd1e4b2fee361eca07193e26cd2d7b2ac5
CrispyCroissant <gentlemug00@gmail.com>
2021-12-17 19:32:48 +0100

    Added confirmation mail sending to /register

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const sendConfirmationMail = require("../utils/mail");
 
 router.post("/register", async (req, res) => {
     const { email, password } = req.body;
@@ -19,6 +20,15 @@ router.post("/register", async (req, res) => {
         await user.save();
     } catch (error) {
         return res.status(400).send({ error: error.message });
+    }
+
+    try {
+        await sendConfirmationMail({
+            email: user.email,
+            id: user._id,
+        });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
     }
 
     res.send({ status: 200 });
