882033544df7eb6edf529b3733f7244e11ccb070
CrispyCroissant <gentlemug00@gmail.com>
2021-12-20 16:51:51 +0100

    Implemented POST /recipe

--- backend/src/__test__/routes/recipe.test.js
+++ backend/src/__test__/routes/recipe.test.js
@@ -1,6 +1,14 @@
 const request = require("supertest");
 const mockingoose = require("mockingoose");
+const User = require("../../models/user");
 const app = require("../../app");
+
+jest.mock("../../middleware/sessionAuth", () =>
+    jest.fn((req, _, next) => {
+        req.session.userID = 1;
+        next();
+    })
+);
 
 describe("POST /api/recipe", () => {
     const route = "/api/recipe";
@@ -8,7 +16,30 @@ describe("POST /api/recipe", () => {
     it("should exist", async () => {
         const res = await request(app).post(route);
 
+        expect(res.status).toBe(400);
+    });
+
+    it("should return 200 if everything went OK", async () => {
+        mockingoose(User).toReturn(
+            {
+                email: "test@email.com",
+                password: "blahblah",
+                emailConfirmed: true,
+            },
+            "findOne"
+        );
+
+        const res = await request(app).post(route).send({ recipe: "Pasta" });
+
         expect(res.status).toBe(200);
+        expect(res.body.recipes).toContain("Pasta");
+    });
+
+    it("should return 400 if recipe was not provided", async () => {
+        const res = await request(app).post(route).send();
+
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe("No recipe was provided!");
     });
 });
 
--- backend/src/routes/recipe.js
+++ backend/src/routes/recipe.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const sessionAuth = require("../middleware/sessionAuth");
+const user = require("../models/user");
 
-router.post("/recipe", (req, res) => {
-    res.send();
+router.post("/recipe", sessionAuth, async (req, res) => {
+    const { recipe } = req.body;
+    const { userID } = req.session;
+
+    if (!recipe) {
+        return res.status(400).send({ error: "No recipe was provided!" });
+    }
+
+    try {
+        const user = await User.findById(userID);
+        user.recipes.push(recipe);
+        await user.save();
+
+        res.send({ recipes: user.recipes });
+    } catch (error) {
+        return res.status(400).send({ error: error.message });
+    }
 });
 
 router.get("/recipes", (req, res) => {
