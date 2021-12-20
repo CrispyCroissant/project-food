d0092ba84409f91c9e348db4e2a35384ea347fd2
CrispyCroissant <gentlemug00@gmail.com>
2021-12-20 16:51:26 +0100

    Added authentication check. Removed redundant user assignment

--- backend/src/middleware/sessionAuth.js
+++ backend/src/middleware/sessionAuth.js
@@ -16,11 +16,11 @@ async function sessionAuth(req, res, next) {
                     .status(401)
                     .send({ error: "User must confirm their email." });
             }
-            
-            session.user = user;
         } catch (error) {
             return res.status(401).send({ error: error.message });
         }
+    } else {
+        return res.status(401).send({ error: "User unauthorized" });
     }
 
     next();
