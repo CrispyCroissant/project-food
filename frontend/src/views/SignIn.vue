b71b0d004ce515a2d6445968d44046289d495b75
CrispyCroissant <gentlemug00@gmail.com>
2021-12-22 13:16:21 +0100

    Changing dev PC

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>test</div>
+  <v-container fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="6">
+        <v-form>
+          <v-text-field solo autofocus label="Email"></v-text-field>
+          <v-text-field solo type="password" label="Password"></v-text-field>
+          <div class="d-flex justify-center">
+            <v-btn>Sign in </v-btn>
+          </div>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
-  export default {
-    name: 'SignIn',
-  }
+export default {
+  name: "SignIn",
+};
 </script>
+
+<style scoped>
+.row {
+  height: 100vh;
+}
+</style>
