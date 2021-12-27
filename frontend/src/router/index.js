d2f14417b82952da6dc5c0267e1a424aebe8979a
CrispyCroissant <gentlemug00@gmail.com>
2021-12-27 16:21:52 +0100

    Added sign up page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import SignIn from '../views/SignIn.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import SignIn from "../views/SignIn.vue";
+import SignUp from "../views/SignUp.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'SignIn',
-    component: SignIn
+    path: "/",
+    name: "SignIn",
+    component: SignIn,
   },
-]
+  {
+    path: "/register",
+    name: "SignUp",
+    component: SignUp,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ frontend/src/views/SignUp.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid class="d-flex flex-column">
+    <v-row id="form-row" class="flex-grow-1" justify="center" align="center">
+      <v-col cols="10" md="6">
+        <h1 class="text-h2 text-md-h1 text-center mb-15">Project Food</h1>
+        <the-sign-up-form></the-sign-up-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import TheSignUpForm from "@/components/SignUp/TheSignUpForm.vue";
+
+export default {
+  name: "SignUp",
+  components: { TheSignUpForm },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+}
+</style>
