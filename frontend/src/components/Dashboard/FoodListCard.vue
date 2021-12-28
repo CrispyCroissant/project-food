211182f92afc418a854ca68a095f3e8c28be8ea1
CrispyCroissant <gentlemug00@gmail.com>
2021-12-28 18:20:47 +0100

    Working on the food list card. not finished...

---
+++ frontend/src/components/Dashboard/FoodListCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card elevation="3" outlined shaped>
+    <v-card-title>Your foods</v-card-title>
+    <v-card-text>
+      <v-list>
+        <v-list-item class="px-0">
+          <v-list-item-content class="mr-4">Test 1</v-list-item-content>
+          <v-list-item-icon>
+            <v-btn icon>
+              <v-icon size="20px" color="red">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+    <v-card-actions>
+      <v-dialog
+        v-model="dialog"
+        transition="dialog-top-transition"
+        max-width="700"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon color="green">mdi-plus</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-actions>
+            <v-text-field label="Add food"></v-text-field>
+            <v-btn class="mx-3" color="primary">Add</v-btn>
+            <v-btn small icon class="align-self-start" @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "FoodListCard",
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
