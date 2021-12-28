7c337cba5cfec4f10e4991f7fd7050e8884ff3e0
CrispyCroissant <gentlemug00@gmail.com>
2021-12-28 18:19:51 +0100

    Created a header component

---
+++ frontend/src/components/nav/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>
+      <h2 class="text-h5">Project Food</h2>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-btn icon ref="logOutBtn" @click="logout">
+      <v-icon color="primary">mdi-logout</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+  methods: {
+    // TODO: Make a logout call to the backend too
+    async logout() {
+      await this.$router.push({ name: "SignIn" });
+    },
+  },
+};
+</script>
