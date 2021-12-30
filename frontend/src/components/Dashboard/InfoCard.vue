13dd9c8e59a7552d21fdd6f1549c3144296d6026
CrispyCroissant <gentlemug00@gmail.com>
2021-12-30 10:55:11 +0100

    Created an info card for the dasboard

---
+++ frontend/src/components/Dashboard/InfoCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card elevation="3" outlined shaped :loading="loading">
+    <v-expand-transition>
+      <div v-if="error">
+        <v-alert ref="errorAlert" type="error" text>
+          {{ error }}
+        </v-alert>
+      </div>
+    </v-expand-transition>
+    <p ref="titleText" class="text-caption text-center my-5 px-5">
+      {{ titleText }}
+    </p>
+    <h3 v-if="recipe" ref="recipeText" class="text-h6 text-center mt-5 px-5">
+      {{ recipe }}
+    </h3>
+    <v-row v-if="recipe" justify="center" class="my-8">
+      <v-btn
+        ref="recipeBtn"
+        class="mx-15"
+        color="primary"
+        @click="getRandomRecipe"
+      >
+        Another one
+      </v-btn>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "InfoCard",
+  data() {
+    return {
+      loading: false,
+      error: false,
+      recipe: "",
+    };
+  },
+  computed: {
+    titleText() {
+      return this.recipe ? "Your next recipe is" : "You have no recipes yet!";
+    },
+  },
+  methods: {
+    getRandomRecipe() {
+      const recipes = this.$store.state.recipes;
+      const randomIndex = Math.floor(Math.random() * recipes.length);
+      const randomRecipe = recipes[randomIndex];
+
+      randomRecipe ? (this.recipe = randomRecipe) : null;
+    },
+  },
+  created() {
+    this.getRandomRecipe();
+  },
+};
+</script>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -3,7 +3,12 @@
     <the-header></the-header>
     <v-container fluid class="d-flex flex-column flex-grow-1">
       <v-row justify="center" align="center">
-        <v-col cols="12" class="d-flex justify-space-between flex-grow-1">
+        <v-col cols="12" class="d-flex justify-center flex-grow-1">
+          <info-card></info-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col cols="12" class="d-flex justify-center flex-grow-1">
           <food-list-card></food-list-card>
         </v-col>
       </v-row>
@@ -13,10 +18,11 @@
 
 <script>
 import FoodListCard from "../components/Dashboard/FoodListCard.vue";
+import InfoCard from "../components/Dashboard/InfoCard.vue";
 import TheHeader from "../components/nav/TheHeader.vue";
 
 export default {
   name: "Dashboard",
-  components: { TheHeader, FoodListCard },
+  components: { TheHeader, FoodListCard, InfoCard },
 };
 </script>
