c059c095bda79ae2eabd26d0916a875eeaf9a16d
CrispyCroissant <gentlemug00@gmail.com>
2022-01-10 17:39:55 +0100

    Added global axios interceptor which sends the current locale as a header.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,14 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import axios from "axios";
-import i18n from './i18n'
+import i18n from "./i18n";
 
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use((config) => {
+  config.headers["Accept-Language"] = store.state.locale;
+  return config;
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
@@ -15,5 +20,5 @@ new Vue({
   vuetify,
   axios,
   i18n,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
